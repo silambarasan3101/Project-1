94c590b1cdad409df6c54086fc625ea45bf8a22f
Silambarasan <msilambarasan98@gmail.com>
2023-09-18 18:33:51 +0530

    first commit

---
+++ Project-1/Project-1/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
